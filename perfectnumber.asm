d0edbe182babd98855aea181ef6d91f33bd97f93
Andrew <rolyata47@gmail.com>
2021-08-25 14:25:10 -0500

    Checks for whether 496 is a perfect number

--- perfectnumber.asm
+++ perfectnumber.asm
@@ -7,19 +7,18 @@ start:
         xor r9, r9
 L:
         inc rbx
-        cmp rbx, 14
+        cmp rbx, 248
         jg print
         xor rdx, rdx
-        mov rax, 28
+        mov rax, 496
         div rbx
         cmp rdx, 0
         jnz L
         add r9, rbx
         jmp L
 print:
-        sub rsp, 8
         mov rdi, message
-        mov rsi, 28
+        mov rsi, 496
         mov rdx, r9
         call _printf
         mov rax, 0x02000001
